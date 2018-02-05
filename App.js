e45d9a639b167c6c561a8ea02354da2be32faaf6
Moises Dobarganes <designerexpert@gmail.com>
2018-02-05 14:48:56 -0500

    Finished

--- App.js
+++ App.js
@@ -17,12 +17,16 @@ export default class App extends React.Component {
     };
   }
 
+  componentDidMount() {
+    // console.log(this.state.todos)
+  }
+
   handleButtonPress = () => {
     this.setState(prevState => {
       let { text, todos } = prevState;
       return {
         text: '',
-        todos: [...todos, { key: text + todos.length, text /* completed */ }]
+        todos: [...todos, { key: text + todos.length, text, completed: false }]
       };
     });
     console.log(this.state.todos);
@@ -32,30 +36,54 @@ export default class App extends React.Component {
     this.setState({ text });
   };
 
+  handleComplete = (id) => {
+    console.log(id)
+    this.setState(prevState => {
+      let oldTodos = prevState.todos;
+      let comp = oldTodos[id].completed;
+      oldTodos[id].completed = !comp;
+      return {
+        todos: oldTodos
+      }
+    })
+    console.log(this.state.todos)
+    this.forceUpdate();
+  }
+
   render() {
     return (
       <View style={container}>
         {this.state.todos.length === 0 ? (
           <Text style={textFont}>You're free</Text>
         ) : (
-          <Text style={textFont}>You got stuff to do!</Text>
-        )}
+            <Text style={textFont}>You got stuff to do!</Text>
+          )}
         <TextInput
           onChangeText={this.handleTextChange}
           value={this.state.text}
           placeholder="Add Todo"
         />
         <Button onPress={() => this.handleButtonPress()} title="Add Todo" />
-        <FlatList
+        <FlatList extraData={this.state}
           data={this.state.todos}
-          renderItem={({ item, key }) => {
-            return (
-              <View key={item.key}>
-                <Text style={/* fill this in with a dynamic style*/ null}>
-                  {item.text}
-                </Text>
-              </View>
-            );
+          renderItem={({ item, index }) => {
+            if (!item.completed) {
+              return (
+                <View key={item.key} id={index}>
+                  <Text style={styles.uncomplete} onPress={() => this.handleComplete(index)}>
+                    {item.text}
+                  </Text>
+                </View>
+              );
+            } else {
+              return (
+                <View key={item.key} id={index}>
+                  <Text style={styles.completed} onPress={() => this.handleComplete(index)}>
+                    {item.text} {'Completed!'}
+                  </Text>
+                </View>
+              );
+            }
           }}
         />
       </View>
@@ -73,7 +101,18 @@ const styles = StyleSheet.create({
   },
   textFont: {
     fontSize: 28
-  }
+  },
+  uncomplete: {
+    fontWeight: 'normal',
+    fontStyle: 'normal',
+    lineHeight: 20,
+
+  },
+  completed: {
+    fontWeight: '900',
+    fontStyle: 'italic',
+    lineHeight: 20,
+  },
 });
 
 const { container, textFont } = styles;
